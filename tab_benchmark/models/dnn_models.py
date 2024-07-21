354a20476d89b10fffc16f5f0acd43689a454382
Bruno <bruno.belucci-teixeira@dauphine.eu>
2024-07-21 21:08:14 +0200

    define other dnn models

--- tab_benchmark/models/dnn_models.py
+++ tab_benchmark/models/dnn_models.py
@@ -1,12 +1,142 @@
 import torch
+from tab_benchmark.dnns.architectures import Node, Saint, TabTransformer, TabNet
 from tab_benchmark.dnns.architectures.mlp import MLP
+from tab_benchmark.dnns.architectures.resnet import ResNet
+from tab_benchmark.dnns.architectures.transformer import Transformer
 from tab_benchmark.models.dnn_model import DNNModel
 from tab_benchmark.models.factories import TabBenchmarkModelFactory
 from tab_benchmark.models.xgboost import fn_to_run_before_fit_for_gbdt
 
+
 MLPModel = TabBenchmarkModelFactory.from_sk_cls(
     DNNModel,
     dnn_architecture_cls=MLP,
+    add_lr_and_weight_decay_params=True,
+    has_auto_early_stopping=True,
+    map_task_to_default_values={
+        'classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'binary_classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'regression': {'loss_fn': torch.nn.functional.mse_loss},
+        'multi_regression': {'loss_fn': torch.nn.functional.mse_loss},
+    },
+    extended_init_kwargs={
+        'categorical_type': 'int64',
+        'categorical_target_type': 'int64',
+        'data_scaler': 'standard',
+        'continuous_target_scaler': 'standard',
+    },
+    fn_to_run_before_fit=fn_to_run_before_fit_for_gbdt,
+)
+
+
+ResNetModel = TabBenchmarkModelFactory.from_sk_cls(
+    DNNModel,
+    dnn_architecture_cls=ResNet,
+    add_lr_and_weight_decay_params=True,
+    has_auto_early_stopping=True,
+    map_task_to_default_values={
+        'classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'binary_classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'regression': {'loss_fn': torch.nn.functional.mse_loss},
+        'multi_regression': {'loss_fn': torch.nn.functional.mse_loss},
+    },
+    extended_init_kwargs={
+        'categorical_type': 'int64',
+        'categorical_target_type': 'int64',
+        'data_scaler': 'standard',
+        'continuous_target_scaler': 'standard',
+    },
+    fn_to_run_before_fit=fn_to_run_before_fit_for_gbdt,
+)
+
+
+TransformerModel = TabBenchmarkModelFactory.from_sk_cls(
+    DNNModel,
+    dnn_architecture_cls=Transformer,
+    add_lr_and_weight_decay_params=True,
+    has_auto_early_stopping=True,
+    map_task_to_default_values={
+        'classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'binary_classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'regression': {'loss_fn': torch.nn.functional.mse_loss},
+        'multi_regression': {'loss_fn': torch.nn.functional.mse_loss},
+    },
+    extended_init_kwargs={
+        'categorical_type': 'int64',
+        'categorical_target_type': 'int64',
+        'data_scaler': 'standard',
+        'continuous_target_scaler': 'standard',
+    },
+    fn_to_run_before_fit=fn_to_run_before_fit_for_gbdt,
+)
+
+
+NodeModel = TabBenchmarkModelFactory.from_sk_cls(
+    DNNModel,
+    dnn_architecture_cls=Node,
+    add_lr_and_weight_decay_params=True,
+    has_auto_early_stopping=True,
+    map_task_to_default_values={
+        'classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'binary_classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'regression': {'loss_fn': torch.nn.functional.mse_loss},
+        'multi_regression': {'loss_fn': torch.nn.functional.mse_loss},
+    },
+    extended_init_kwargs={
+        'categorical_type': 'float32',
+        'categorical_target_type': 'int64',
+        'data_scaler': 'standard',
+        'continuous_target_scaler': 'standard',
+    },
+    fn_to_run_before_fit=fn_to_run_before_fit_for_gbdt,
+)
+
+
+SaintModel = TabBenchmarkModelFactory.from_sk_cls(
+    DNNModel,
+    dnn_architecture_cls=Saint,
+    add_lr_and_weight_decay_params=True,
+    has_auto_early_stopping=True,
+    map_task_to_default_values={
+        'classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'binary_classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'regression': {'loss_fn': torch.nn.functional.mse_loss},
+        'multi_regression': {'loss_fn': torch.nn.functional.mse_loss},
+    },
+    extended_init_kwargs={
+        'categorical_type': 'float32',
+        'categorical_target_type': 'int64',
+        'data_scaler': 'standard',
+        'continuous_target_scaler': 'standard',
+    },
+    fn_to_run_before_fit=fn_to_run_before_fit_for_gbdt,
+)
+
+
+TabTransformerModel = TabBenchmarkModelFactory.from_sk_cls(
+    DNNModel,
+    dnn_architecture_cls=TabTransformer,
+    add_lr_and_weight_decay_params=True,
+    has_auto_early_stopping=True,
+    map_task_to_default_values={
+        'classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'binary_classification': {'loss_fn': torch.nn.functional.cross_entropy},
+        'regression': {'loss_fn': torch.nn.functional.mse_loss},
+        'multi_regression': {'loss_fn': torch.nn.functional.mse_loss},
+    },
+    extended_init_kwargs={
+        'categorical_type': 'float32',
+        'categorical_target_type': 'int64',
+        'data_scaler': 'standard',
+        'continuous_target_scaler': 'standard',
+    },
+    fn_to_run_before_fit=fn_to_run_before_fit_for_gbdt,
+)
+
+
+TabNetModel = TabBenchmarkModelFactory.from_sk_cls(
+    DNNModel,
+    dnn_architecture_cls=TabNet,
     add_lr_and_weight_decay_params=True,
     has_auto_early_stopping=True,
     map_task_to_default_values={
