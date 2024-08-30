e5bfd2d5b4d2db891f90f413ba7bef36191a6c27
Bruno <bruno.belucci-teixeira@dauphine.eu>
2024-08-30 15:37:26 +0200

    fix indentation

--- run_scripts/generate_db_script.py
+++ run_scripts/generate_db_script.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from inspect import cleandoc
 
 
 def generate_postgres_db_script(
@@ -15,7 +16,7 @@ def generate_postgres_db_script(
         wall_time='364-23:59:59',
 ):
     log_file = database_dir / (db_name + '.log')
-    sh_content = f"""
+    sh_content = cleandoc(f"""
     if [ ! -d {str(database_dir.absolute())} ]; then
         conda run -n {conda_env} initdb -D {str(database_dir.absolute())}
         echo "host	all	all	samenet	trust" >> {str(database_dir.absolute())}/pg_hba.conf
@@ -23,16 +24,17 @@ def generate_postgres_db_script(
     conda run -n {conda_env} pg_ctl -D {str(database_dir.absolute())} -l {str(log_file.absolute())} -o "-h 0.0.0.0 -p {db_port}" start
     conda run -n {conda_env} createdb {db_name} -p {db_port}
     conda run -n {conda_env} mlflow server --backend-store-uri postgresql://localhost:{db_port}/{db_name} -h 0.0.0.0 -p {mlflow_port}
-    """
+    """) + '\n'
     if generate_sbatch:
-        sbatch_content = f"""#!/bin/sh
+        sbatch_content = cleandoc(f"""
+        #!/bin/sh
         #SBATCH -c {n_cores}
         #SBATCH -w {clust_name}
         #SBATCH --job-name={job_name}
         #SBATCH --output={output_job_dir}
         #SBATCH --error={error_job_dir}
         #SBATCH --time={wall_time}
-        """
+        """) + '\n'
         file_content = sbatch_content + sh_content
         file_ext = '.sbatch'
     else:
