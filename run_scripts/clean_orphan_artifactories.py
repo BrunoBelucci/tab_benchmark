db4e9a65ea0a00e4d0c81ff91dc283b6bdd26194
Bruno <bruno.belucci-teixeira@dauphine.eu>
2024-11-12 14:53:02 +0100

    add possibility to clean using sql (faster)

--- run_scripts/clean_orphan_artifactories.py
+++ run_scripts/clean_orphan_artifactories.py
@@ -1,14 +1,22 @@
 import mlflow
+from sqlalchemy import create_engine
+import pandas as pd
 from pathlib import Path
 from shutil import rmtree
 import argparse
 
 
-def clean_orphan_artifactory(mlflow_tracking_uri, artifactory_root_dir):
-    mlflow.set_tracking_uri(mlflow_tracking_uri)
-    experiments = mlflow.search_experiments()
+def clean_orphan_artifactory(tracking_url, artifactory_root_dir, use_sql=True):
     # get all experiments artifactories location
-    experiment_artifactories = {Path(exp.artifact_location).resolve() for exp in experiments}
+    if not use_sql:
+        mlflow.set_tracking_uri(tracking_url)
+        experiments = mlflow.search_experiments()
+        experiment_artifactories = {Path(exp.artifact_location).resolve() for exp in experiments}
+    else:
+        engine = create_engine(tracking_url)
+        query = 'SELECT experiments.artifact_location from experiments'
+        experiment_artifactories = set(pd.read_sql(query, engine)['artifact_location'].apply(
+            lambda x: Path(x).resolve()).tolist())
     # get all dir that should represent an artifactory location
     experiment_existent_artifactories = {Path(art_dir).resolve() for art_dir in artifactory_root_dir.iterdir()}
     # get what exists in the filesystem but not in the mlflow tracking server
@@ -18,10 +26,18 @@ def clean_orphan_artifactory(mlflow_tracking_uri, artifactory_root_dir):
         rmtree(orphan_artifactory)
 
     # now do the same for runs inside the experiments
-    runs = mlflow.search_runs(search_all_experiments=True)
-    # get all runs artifactories location
-    runs_artifactories = {Path(run.artifact_uri).parent.resolve() for run in runs.itertuples()}
-    runs_existent_artifactories = {run_dir.resolve() for experiment_dir in artifactory_root_dir.iterdir() for run_dir in experiment_dir.iterdir()}
+    if not use_sql:
+        runs = mlflow.search_runs(search_all_experiments=True)
+        # get all runs artifactories location
+        runs_artifactories = {Path(run.artifact_uri).parent.resolve() for run in runs.itertuples()}
+    else:
+        query = 'SELECT runs.artifact_uri from runs'
+        runs_artifactories = set(pd.read_sql(query, engine)['artifact_uri'].apply(
+            lambda x: Path(x).parent.resolve()).tolist())
+
+    runs_existent_artifactories = {run_dir.resolve()
+                                   for experiment_dir in artifactory_root_dir.iterdir()
+                                   for run_dir in experiment_dir.iterdir()}
     orphan_runs_artifactories = runs_existent_artifactories - runs_artifactories
     for orphan_run_artifactory in orphan_runs_artifactories:
         rmtree(orphan_run_artifactory)
