70a674a3dfb87d886313aa4b3db456790f2d445a
Bruno <bruno.belucci-teixeira@dauphine.eu>
2024-11-15 11:27:17 +0100

    add parameter to only log at each epoch interval

--- tab_benchmark/dnns/callbacks/default_log.py
+++ tab_benchmark/dnns/callbacks/default_log.py
@@ -7,13 +7,14 @@ class DefaultLogs(Callback):
     This callback logs the losses and the global step at the end of each epoch for the train, validation and test sets.
     It can manage multiple dataloaders (multiple train, validation and test sets).
     """
-    def __init__(self, on_epoch=True, on_step=False):
+    def __init__(self, on_epoch=True, on_step=False, log_interval_epoch=1):
         self.on_epoch = on_epoch
         self.on_step = on_step
+        self.log_interval_epoch = log_interval_epoch
         self.pruned_trial = False
 
     def on_batch_end(self, str_set, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
-        if not trainer.sanity_checking:
+        if not trainer.sanity_checking and trainer.current_epoch % self.log_interval_epoch == 0:
             dataset_name = None
             if hasattr(trainer.datamodule, str_set + '_datasets'):
                 dataset_name = list(getattr(trainer.datamodule, str_set + '_datasets').keys())[dataloader_idx]
@@ -22,7 +23,6 @@ class DefaultLogs(Callback):
                     dataset_name = getattr(trainer.datamodule, str_set + '_dataset').name
             if dataset_name is None:
                 dataset_name = str_set + '_' + str(dataloader_idx)
-            pl_module.log('global_step', float(trainer.global_step), on_epoch=self.on_epoch, on_step=self.on_step)
             pl_module.log(dataset_name + '_loss', outputs['loss'], on_epoch=self.on_epoch, on_step=self.on_step,
                           prog_bar=True, logger=True)
 
